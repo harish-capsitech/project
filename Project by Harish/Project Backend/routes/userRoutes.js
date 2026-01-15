08a00afcbd91244195f44fed8555381ba2094f2b
harish-capsitech <harish_kumar@capsitech.com>
2026-01-15 15:03:40 +0530

    Delete Project by Harish directory

--- Project by Harish/Project Backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import expressRouter from "express";
-export const router = expressRouter.Router();
-
-import {verifyToken} from "../middleware/verifyToken.js"
-import { getUser, registerUser, loginUser } from "../controllers/userController.js";
-router.get("/users", verifyToken, getUser);
-router.post("/signup", registerUser);
-router.post("/login", loginUser);
-
