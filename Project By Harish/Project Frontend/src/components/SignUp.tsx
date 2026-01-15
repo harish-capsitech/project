9cc3d5b297d9d6a5f6e0ecdde4df9da5ad61afed
harish-capsitech <harish_kumar@capsitech.com>
2026-01-15 15:07:25 +0530

    Update SignUp.tsx

--- Project By Harish/Project Frontend/src/components/SignUp.tsx
+++ Project By Harish/Project Frontend/src/components/SignUp.tsx
@@ -1,5 +1,8 @@
 import { Link, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+
 const SignUp = () => {
     const [error, setError] = useState<string | null>(null);
     const [nameError, setNameError] = useState<string | null>(null);
@@ -7,6 +10,7 @@ const SignUp = () => {
     const [emailError, setEmailError2] = useState<string | null>(null);
     const [emailError2, setEmailError] = useState<string | null>(null);
     const [successMsg, setSuccessMsg] = useState<string | null>(null);
+    const [passwordVisible, setPasswordVisible] = useState(false);
     const [userData, setUserData] = useState<{fullName: string; email: string; password: string} | null>({fullName: "", email: "", password: ""});
     const navigate = useNavigate();
     const handleSignUp = async (e: React.FormEvent) => {
@@ -80,6 +84,9 @@ const SignUp = () => {
             navigate("/");
         }
     });
+    const passwordShow = () => {
+    setPasswordVisible(!passwordVisible);
+  };
   return (
     <main>
         <div className="bg-gray-200 h-screen w-full flex items-center justify-center">
@@ -95,14 +102,21 @@ const SignUp = () => {
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-gray-700">Email</label>
                         <input type="email" name="email" onChange={(e)=>setUserData({...(userData || {fullName: "", email: "", password: ""}), email:e.target.value})} id="email" className="w-full px-3 py-2 border rounded" />
+                        
                         <p className="text-red-500">
                             {emailError} {emailError2}
                         </p>
                        
                     </div>
-                    <div className="mb-4">
+                    <div className="mb-4 relative">
                         <label htmlFor="password" className="block text-gray-700">Password</label>
-                        <input type="password" name="password" onChange={(e)=>setUserData({...(userData || {fullName: "", email: "", password: ""}), password:e.target.value})} id="password" className="w-full px-3 py-2 border rounded" />
+                        <input type={passwordVisible ? "text" : "password"} name="password" onChange={(e)=>setUserData({...(userData || {fullName: "", email: "", password: ""}), password:e.target.value})} id="password" className="w-full px-3 py-2 border rounded" />
+                        <span onClick={passwordShow} className="absolute right-5 bottom-2.5">
+                            <FontAwesomeIcon
+                            icon={passwordVisible ? faEyeSlash : faEye}
+                            className="cursor-pointer"
+                            />
+                        </span>
                         <p className="text-red-500">{passError}</p>
                     </div>
                     <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600" >Sign Up</button>
